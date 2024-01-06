e77e0d19ef53ebd683785b9e24582b8920d8acce
Sathish Jayapal <sjayapal@skminfotech.com>
2024-01-06 17:27:47 -0600

    strava tracker skeleton

---
+++ trackstrava/src/test/java/me/sathish/trackstrava/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.sathish.trackstrava;
+
+import me.sathish.trackstrava.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
