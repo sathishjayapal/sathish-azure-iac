8d46aea6e2dec582936bf964487b4a44a984e39a
Sathish Jayapal <sjayapal@skminfotech.com>
2024-01-06 18:13:38 -0600

    garmin and meditation tracker

---
+++ trackmeditation/src/test/java/me/sathish/trackmeditation/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.sathish.trackmeditation;
+
+import me.sathish.trackmeditation.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ trackmeditation/src/test/java/me/sathish/trackmeditation/TestApplication.java
@@ -0,0 +1,12 @@
+package me.sathish.trackmeditation;
+
+import me.sathish.trackmeditation.common.ContainersConfig;
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "local");
+        SpringApplication.from(Application::main).with(ContainersConfig.class).run(args);
+    }
+}
